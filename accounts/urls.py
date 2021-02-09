cadc48b857fc4233b66d0985bcb48e45216ba458
halkayed <halkayed@htmail.com>
2021-02-09 20:04:30 +0200

    accounts app

---
+++ accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import signup
+
+
+from django.contrib.auth.views import LoginView, LogoutView
+
+LoginView.template_name = 'accounts/login.html'
+
+
+urlpatterns = [
+    path('signup/', signup, name='signup'),
+    path('login/',  LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
