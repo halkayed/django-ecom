06fb40ec2a778f2465e30f4a2b2b0419a72a03c5
halkayed <halkayed@htmail.com>
2021-02-15 21:23:17 +0200

    static_root defined

--- store/settings.py
+++ store/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 STATICFILES_DIRS =[
     BASE_DIR.joinpath('static')
     #BASE_DIR.joinpath('static/CSS/'),
